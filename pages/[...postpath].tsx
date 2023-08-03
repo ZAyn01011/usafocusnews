b459c9763ff8f3b1dc2bb82b7b97e1041e037497
ZAyn01011 <141336550+ZAyn01011@users.noreply.github.com>
2023-08-03 18:30:15 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://newstabe.com/` + encodeURI(path as string)
+					`https://usafocusnews.com//` + encodeURI(path as string)
 				}`,
 			},
 		};
